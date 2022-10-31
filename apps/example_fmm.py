6e65209ff722bc0b966d9f0eca3170f40d5ec346
Callum Marples <cmarples@hotmail.co.uk>
2022-10-31 16:25:09 +0000

    Closest point on triangulation.

--- apps/example_fmm.py
+++ apps/example_fmm.py
@@ -14,6 +14,7 @@ os.chdir("..")
 
 import math
 import time
+import numpy as np
 
 from leod.ellipsoid_shape import EllipsoidShape
 from leod.fmm_vertex import FmmVertex
@@ -23,16 +24,39 @@ from leod.fmm_fast_marching import fmm_idw
 from leod.sphere_geodesics import great_circle_distance
 from leod.triaxial_geodesics import boundary_value_method
 from leod.triangulation_sphere import triangulate_sphere
-from leod.triangulation import check_triangles
+
+import leod.triangulation as tri
 
 test_no = 1
 
 if test_no == 1: # FMM on sphere triangulation
     
     r = 1.0 # radius
+    shape = EllipsoidShape(1.0, 1.0, 1.0)
     n = 100  # number of triangular divisions
     tic = time.perf_counter()
     vertex = triangulate_sphere(r, n)
     toc = time.perf_counter()
     print(toc - tic)
-    no_obtuse = check_triangles(vertex)
+    no_obtuse = tri.check_triangles(vertex)
+    
+    # Closest point to given start
+    start_th = 90.0 * math.pi / 180.0
+    start_ph = 0.0  * math.pi / 180.0  
+    p_start = shape.polar2cart(start_th, start_ph)
+    p_start = np.array(p_start)
+    end_th = 50.0 * math.pi / 180.0
+    end_ph = 60.0 * math.pi / 180.0
+    p_end = shape.polar2cart(end_th, end_ph)
+    p_end = np.array(p_end)
+    
+    tic = time.perf_counter()
+    v_start = tri.find_closest_vertex(vertex, p_start)
+    v_end = tri.find_closest_vertex(vertex, p_end)
+    toc = time.perf_counter()
+    print(toc - tic)
+    
+    fmm = fast_marching(vertex, v_start, 1)
+    d = fmm.distance[v_end]
+    
+    s = great_circle_distance(1.0, start_th, start_ph, end_th, end_ph)
--- leod/triangulation.py
+++ leod/triangulation.py
@@ -8,6 +8,44 @@ Created on Mon Sep 26 12:31:45 2022
 import math
 import numpy as np
 
+# Find closest vertex to input point
+def find_closest_vertex(vertex, p, v=0):
+
+    d2 = {}
+    is_min_found = False
+    while is_min_found == False:
+        if v not in d2.keys():
+            vec = vertex[v].carts - p
+            d2[v] = np.dot(vec, vec)
+        u = v # Index of locally closest vertex
+        d_min = d2[v]
+        for j in vertex[v].distance_to_neighbour.keys():
+            if j not in d2.keys():
+                vec = vertex[j].carts - p
+                d2[j] = np.dot(vec, vec)
+            if d2[j] < d_min:
+                d_min = d2[j]
+                u = j
+        if u == v:
+            is_min_found = True
+        else:
+            v = u
+    
+    return v
+    
+    
+    '''
+    d_min = math.inf
+    v_min = 0
+    for i in range(len(vertex)):
+        vec = vertex[i].carts - p
+        d2 = np.dot(vec, vec)
+        if d2 < d_min:
+            d_min = d2
+            v_min = i
+    return v_min
+    '''
+
 # Check that each triangle is valid by calculating each vertex angle
 def check_triangles(vertex):
     no_obtuse = 0
